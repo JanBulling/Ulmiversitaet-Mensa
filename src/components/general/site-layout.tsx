5f9616e64b0fa26f36d236cb7080df25c352b9a0
Jan Bulling <jan.greenengage@gmail.com>
2025-08-17 16:52:08 +0200

    fix: SiteLayout props definition

--- src/components/general/site-layout.tsx
+++ src/components/general/site-layout.tsx
@@ -1,12 +1,10 @@
 import { cn } from "@/lib/utils";
 
-interface SiteLayoutProps extends React.ComponentProps<"div"> {}
-
 export default function SiteLayout({
   children,
   className,
   ...props
-}: SiteLayoutProps) {
+}: React.ComponentProps<"div">) {
   return (
     <div
       className={cn("mx-auto max-w-screen-xl px-4 md:px-12", className)}
